536415ae9afef6378ba3e85e06bc07bd9ec537a2
beautylight <rwg3597@naver.com>
2019-04-02 14:40:08 +0900

    rewrite

--- 21100506.c
+++ 21100506.c
@@ -3,11 +3,12 @@
 #include <string.h>
 
 int reverse_digits(int n){
-    
-    int reverse = 0;
-    while(n){
-        reverse = (reverse * 10) + (n % 10);
-        n = n/10;
-    }
-    return reverse;
+	
+	int reverse = 0;
+	while(n){
+		reverse = (reverse * 10) + (n % 10);
+		n = n/10;
+	}
+	return reverse;
 }
+
--- main.c
+++ main.c
@@ -10,37 +10,41 @@ typedef int bool ;
 
 void main(){
 
-    char input[2048] ;
-    bool is_number ;
-    int reverse = 0 ;
-    int sort = 0 ;
+	char input[2048] ;
+	bool is_number ;
+	int reverse = 0 ;
+	int sort = 0 ;
 
-    while(fgets(input, sizeof(input), stdin) != NULL){
+	while(fgets(input, sizeof(input), stdin) != NULL){
 
-        //Receive a user input
-        if(input[strlen(input) - 1] == '\n')
-            input[strlen(input) - 1] = 0;
+		//Receive a user input
+		if(input[strlen(input) - 1] == '\n')
+			input[strlen(input) - 1] = 0;
 
-        //Terminal condition
-        if(strcmp(input, "quit") == 0){
-            printf("(bye)\n") ;
-            break ;
-        }
+		//Terminal condition
+		if(strcmp(input, "quit") == 0){
+			printf("(bye)\n") ;
+			break ;
+		}
 
-        is_number = true;
-        for(int i = 0; i < strlen(input); i++){
-            if(!isdigit(input[i])){
-                printf("Entered input is not a number!\n") ;
-                is_number = false ;
-                break;
-            }
-        }
+		is_number = true;
+		for(int i = 0; i < strlen(input); i++){
+			if(!isdigit(input[i])){
+				printf("Entered input is not a number!\n") ;
+				is_number = false ;
+				break;
+			}
+		}
 
-        if(is_number){
-            //Compute and print the length
-            reverse = reverse_digits(atoi(input)) ;
-            sort = sort_digits(atoi(input)) ;
-            printf("reverse> %d\nsort> %d\n", reverse, sort) ;
-        }
-    }
-    return ;
+		if(is_number){
+			//Compute and print the length
+			reverse = reverse_digits(atoi(input)) ;
+			sort = sort_digits(atoi(input)) ;
+			printf("reverse> %d\nsort> %d\n", reverse, sort) ;
+		}
+	}
+
+	return ;
+}
+
+
